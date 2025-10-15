9bf1d2c8c97e0abaa4d4d095688abbd06d6c4784
vik <vik.denic@gmail.com>
2025-10-15 15:01:28 -0500

    Fix Package.swift: use iOS 13 for Swift tools 5.3 compatibility

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MailCore2",
     platforms: [
-        .iOS(.v16), .macOS(.v10_10)
+        .iOS(.v13), .macOS(.v10_10)
     ],
     products: [
         .library(
